2d484d4acf54d8d29ddac94e3e094d28e024c05f
antoabrilx <antoaldalla@gmail.com>
2025-06-15 19:12:31 -0300

    Pagina de inicio de sesion

---
+++ src/PageLogin.jsx
@@ -0,0 +1,33 @@
+import Login from './Login';
+import '../style/PageLogin.css'; 
+import Header from './Header';
+
+function PageLogin() {
+  const handleLogin = (user, pass) => {
+    if (!user || !pass) {
+      alert('Por favor, completa ambos campos.');
+      return;
+    }
+    if (user === 'admin' && pass === 'admin') {
+      alert('Bienvenido, administrador!');
+    }
+    else {
+      alert('Usuario o contraseña incorrectos.');
+    }
+    // Aquí iría la llamada a la API de autenticación
+  };
+
+  return (
+    <div className='page-login'>
+      <Header />
+      <h1>Bienvenido</h1>
+      <div className='formulario'>
+          <Login
+            onLoginSubmit={handleLogin}
+          />
+      </div>
+    </div>
+  );
+}
+
+export default PageLogin;
