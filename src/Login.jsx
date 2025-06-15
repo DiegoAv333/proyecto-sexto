02adefcabb91a133b2d4f2f95fe402c71d52be4e
LuciaMunoz17 <munoz.lucia.epet20@gmail.com>
2025-06-15 17:31:46 -0600

    Login

---
+++ src/Login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import '../style/Login.css';
+
+function Login({onLoginSubmit}) {
+  // Estado interno para los inputs de este formulario
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLoginSubmit(username, password);
+    // limpia los campos después de enviar el login
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className='login-form'>
+      <h2>Iniciar Sesión</h2>
+        <div>
+          <label htmlFor="loginUsername">Username:</label>
+          <input
+            type="text"
+            id="loginUsername"
+            value={username} // <-- El valor proviene del estado interno
+            onChange={(e) => setUsername(e.target.value)} // <-- Actualiza el estado interno
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="loginPassword">Password:</label>
+          <input
+            type="password"
+            id="loginPassword"
+            value={password} // <-- El valor proviene del estado interno
+            onChange={(e) => setPassword(e.target.value)} // <-- Actualiza el estado interno
+            required
+          />
+        </div>
+        <button type="submit">Ingresar</button>
+        <p className='register-option'>¿No tienes una cuenta? <a href="#">Regístrate aquí</a></p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
