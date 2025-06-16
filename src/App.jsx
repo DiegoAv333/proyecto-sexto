c52e585892297ca5854a88bd7e93f38b4acabb21
DiegoAv333 <diego.aviles7978@gmail.com>
2025-06-16 00:44:39 -0300

    Cambios minimos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import HeaderP from "./components//header/HeaderP";
 import FooterP from "./components/footerPage/FooterP";
