b81600270cd730cbd5edc0f7767f5b1ae776d3ba
jotaaaa321 <joacoavi2018@gmail.com>
2025-10-11 23:43:54 -0300

    LogicaAutenticación/Perfil

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -28,4 +28,81 @@ export function AuthProvider({ children }) {
     setLoading(false);
     }, []);
 
- 
+    //funcion del login
+    const login = (email, password) => {
+    const users = getUsers();
+    const found = users.find((u) => u.email === email && u.password === password);
+    if (!found) throw new Error("Email o contraseña incorrectos");
+    
+    setUser(found);
+    setCurrentUser(found);
+
+    //Redirigir al home o dashboard según necesidad
+    navigate("/dashboard");
+    return found;
+    };
+
+    //funcion del register
+    const register = (name, email, password) => {
+        const users = getUsers();
+        if (users.find(u => u.email === email)) throw new Error("Ya existe una cuenta con este email");
+
+  // Nuevo usuario con rol y campos extra inicializados vacíos
+        const newUser = { 
+            name, 
+            email, 
+            password, 
+            role: "frontend", 
+            dni: "", 
+            address: "", 
+            phone: "" 
+        };
+
+    users.push(newUser);
+    setUsers(users);
+    setUser(newUser);
+    setCurrentUser(newUser);
+
+    //redirigir después de registrar
+    navigate("/dashboard");
+    return newUser;
+};
+
+
+    const updateProfile = ({ name, email, password, dni, address, phone }) => {
+        const users = getUsers();
+        const idx = users.findIndex(u => u.email === user.email);
+        if (idx === -1) return;
+
+        const updated = {
+        ...users[idx],    // conserva role y otros
+            name,
+            email,
+            dni,
+        address,
+        phone,
+        ...(password ? { password } : {}),
+    };
+    users[idx] = updated;
+    setUsers(users);
+    setUser(updated);
+    setCurrentUser(updated);
+};
+
+    //cerrar sesión
+    const logout = () => {
+    setUser(null);
+    clearCurrentUser();
+    navigate("/login");//redirige al login al cerrar sesión
+    };
+
+    const enrolledCount = useMemo(
+    () => (user ? getEnrolled(user.email).length : 0),
+    [user]
+    );
+
+    const value = { user, loading, login, register, updateProfile, logout, enrolledCount };
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+//hook
+export const useAuth = () => useContext(AuthContext);
