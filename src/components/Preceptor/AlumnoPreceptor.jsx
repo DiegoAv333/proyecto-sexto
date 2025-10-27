98dbcaf67653a47d512be1a2955edeb9abe58182
Constanza Falleti <costyfalleti.epet20@gmail.com>
2025-10-26 22:41:49 -0300

    Listar Alumnos y agregar funcion Eliminar

--- src/components/Preceptor/AlumnoPreceptor.jsx
+++ src/components/Preceptor/AlumnoPreceptor.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
+import { usePreceptor } from "../context/PreceptorContext";
 
 export default function AlumnosPreceptor() {
+  const { alumnos, eliminarAlumno } = usePreceptor();
   const navigate = useNavigate();
 
 return (
@@ -16,6 +18,31 @@ return (
       </header>
       <h2 className="text-2xl font-bold text-dark-gray">Lista de Alumnos</h2>
 
+    <table className="w-full border-collapse">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="border px-4 py-2">Nombre</th>
+            <th className="border px-4 py-2">Email</th>
+            <th className="border px-4 py-2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {alumnos.map((a) => (
+            <tr key={a.id} className="text-center">
+              <td className="border px-4 py-2">{a.nombre}</td>
+              <td className="border px-4 py-2">{a.email}</td>
+              <td className="border px-4 py-2">
+                <button
+                  onClick={() => eliminarAlumno(a.id)}
+                  className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 transition"
+                >
+                  Eliminar
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
   );
 }
