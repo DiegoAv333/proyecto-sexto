d6e39822735ae57fe58a50caa5b364905745516d
Constanza Falleti <costyfalleti.epet20@gmail.com>
2025-10-26 21:50:19 -0300

    Creando Panel de Preceptores

---
+++ src/components/Preceptor/PreceptorDashboard.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import anim from "../../styles/animations.module.css";
+import { usePreceptor } from "../context/PreceptorContext";
+
+
+export default function PreceptorDashboard() {
+    return(
+        <section>
+      <h1>Panel del Preceptor</h1>
+    </section>
+    )
+}
